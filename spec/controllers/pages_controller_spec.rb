25a9a2fbeb2ce373bc65c91cf42537c5a5a28824
Garrett Smith <garrettsmith@live.ca>
2011-12-01 16:40:46 -0600

    Finished static page section.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,10 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -15,12 +19,20 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have right title" do
+      get 'contact'
+      response.should have_selector("title", :content => " | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+    it "should have right title" do
+      get 'about'
+      response.should have_selector("title", :content => " | About")
     end
   end
 
