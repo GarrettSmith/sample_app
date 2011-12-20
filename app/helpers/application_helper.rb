e7b8e34951878b197b21b49973d5da6109e285cc
Garrett Smith <garrettsmith@live.ca>
2011-12-20 11:52:23 -0600

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return the logo image element
+  def logo
+    image_tag "logo.png", :alt => "Sample App", :class => "round"
+  end
+
   # Return the title of the page
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
