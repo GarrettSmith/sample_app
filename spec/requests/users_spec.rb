1625190d8237ad9bcdeeca8eab221857fbbe3321
Garrett Smith <garrettsmith@live.ca>
2011-12-22 12:21:43 -0600

    User signup complete

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Users" do
+
+  describe "signup" do
+     
+    describe "failure" do
+
+      it "should not make a new user" do
+        proc do
+          visit signup_path
+          fill_in "Name",                   :with => ""
+          fill_in "Email",                  :with => ""
+          fill_in "Password",               :with => ""
+          fill_in "Password confirmation",  :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(User, :count)
+      end
+    end
+
+    describe "success" do
+
+      it "should make a new user" do
+        proc do
+          visit signup_path
+          fill_in "Name",                   :with => "New User"
+          fill_in "Email",                  :with => "user@example.com"
+          fill_in "Password",               :with => "password"
+          fill_in "Password confirmation",  :with => "password"
+          click_button
+          response.should render_template('users/show')
+          response.should have_selector("div.flash.success", 
+                                        :content => "Welcome")
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+end
